94a3c37f28beb69d6308a88b942de33429c93064
nikhi-verma54 <nikkcr3141@gmail.com>
2025-11-26 21:11:34 +0530

    day 1

---
+++ Reverse_a_number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        result = 0
+        while x > 0:
+            digit = x % 10
+            result = result * 10 + digit
+            x //= 10
+
+        result *= sign
+        
+        if result < INT_MIN or result > INT_MAX:
+            return 0
+        
+        return result
+
+
+# Manual input testing
+Solution()
+x = int(input("Enter a number: "))
+print("Reversed:", Solution().reverse(x))
